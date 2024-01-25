6932db9e41d15078e84998927eb488df272d89cf
polsa <pol.sanchez.7e7@itb.cat>
2024-01-25 19:10:05 +0100

    fdf

--- e3.py
+++ e3.py
@@ -1,41 +1,35 @@
 """
 Pol Sánchez, Dídac Lloret, Rubén Sánchez
 ASIXc1B
-Programa de traducció d’insults. Crear una estructura de dues dimensions amb els insults en català i afegir la traducció en castellà, anglès i klingon
+Programa de traducció d’insults. Crear una estructura de dues dimensions amb els insults en català i afegir la traducció en castellà, anglès i nepalí
 """
+CAT = 0
+ESP = 1
+ENG = 2
+NEP = 3
 
-ins = input("Introdueix un insult en català: ")
-try:
-
-    CAT = 0
-    ESP = 1
-    ENG = 2
-    NEP = 3
-
-    insults = [
-        ["Malparit" "Bufetes" "Brètol" "Llanut" "Borinot" "Banyut" "Bagassa" "Beneit" "Llepaculs" "Nyèbit"],
-        ["Malparido" "Bufetes" "Brutote" "Llano" "Tonto, simple" "Cuernudo" "Prostituta" "Bendito" "Lameculos" "Niñato"],
-        ["Bastard" "Lawyers" "Brute" "Flat, Plain" "Silly" "Horned" "Prostitute" "Blessed" "Ass-licker" "Brat"],
-        ["बास्टर्ड" "वकील" "ब्रुट" "फ्लैट, प्लेन" "सिली" "सिङ्ग" "वेश्या" "धन्य" "गधा चाट्ने" "ब्याट"]
-    ]
-
-    # Bucle per demanar insults a l'usuari
+insults = [
+    ["Malparit", "Bufetes", "Brètol", "Llanut", "Borinot", "Banyut", "Bagassa", "Beneit", "Llepaculs", "Nyèbit"],
+    ["Malparido", "Bufetes", "Brutote", "Llano", "Tonto, simple", "Cuernudo", "Prostituta", "Bendito", "Lameculos", "Niñato"],
+    ["Bastard", "Lawyers", "Brute", "Flat, Plain", "Silly", "Horned", "Prostitute", "Blessed", "Ass-licker", "Brat"],
+    ["बास्टर्ड", "वकील", "ब्रुट", "फ्लैट, प्लेन", "सिली", "सिङ्ग", "वेश्या", "धन्य", "गधा चाट्ने", "ब्याट"]
+]
 
 
+while True:
+    insult = input('Introdueix un insult en català (o "exit" per sortir): ')
 
 
+    try:
+        index = insults[CAT].index(insult)
+        traduccio_cat, traduccio_esp, traduccio_eng, traduccio_nep = insults[CAT][index], insults[ESP][index], insults[ENG][index], insults[NEP][index]
 
-    if ins in insults[CAT]:
-        index_insult_cat = insults[CAT].index(ins)
+        print(f'Insult en català: {traduccio_cat}')
+        print(f'Traducció a castellà: {traduccio_esp}')
+        print(f'Traducció a anglès: {traduccio_eng}')
+        print(f'Traducció a nepalí: {traduccio_nep}')
 
-        print(f"En català: {ins}")
-        print(f"En castellà: {insults[ESP][index_insult_cat]}")
-        print(f"En anglès: {insults[ENG][index_insult_cat]}")
-        print(f"En klingon: {insults[NEP][index_insult_cat]}")
-    else:
-        print("Insult no trobat. Prova'n un altre.")
-except ValueError:
-    print("No correct")
-
+    except ValueError:
+        print('Insult no trobat en la llista. Torna-ho a provar.')
 
 
