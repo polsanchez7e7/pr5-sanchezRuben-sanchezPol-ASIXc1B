4b123d5802077a36f87a3e4aaec013c7c797401d
didac.lloret.7e7 <didac.lloret.7e7@itb.cat>
2024-01-25 13:06:29 +0100

    s

--- e2.py
+++ e2.py
@@ -10,12 +10,16 @@ llista_aleatoria = [random.randint(1, 50) for i in range(100)]
 sumparells = 0
 sumsenars = 0
 print("Amb els nombres:", end=' ')
+#bucle que recolecta els parells i els suma
 for i in range(0, 99, 2):
     sumparells = (llista_aleatoria[i]) + sumparells
+#bucle que recolecta els senars i esl suma
 for j in range(1, 99, 2):
     sumsenars = (llista_aleatoria[j]) + sumsenars
+#bucle que printa la llista de numeros aleatoris
 for elemento in llista_aleatoria:
     print(elemento, end=',')
+#calcul de les mitjanes
 mitjana_parells = sumparells / 50
 mitjana_senars = sumsenars / 50
 print()
