5f194b108d1e73a342bceae5d077bbed6546fce9
pol.sanchez.7e7 <pol.sanchez.7e7@itb.cat>
2024-01-25 13:06:48 +0100

    s

--- e3.py
+++ e3.py
@@ -4,12 +4,38 @@ ASIXc1B
 Programa de traducció d’insults. Crear una estructura de dues dimensions amb els insults en català i afegir la traducció en castellà, anglès i klingon
 """
 
-ins = input("Introdueix un insult en català")
+ins = input("Introdueix un insult en català: ")
+try:
 
-incata = ("Malparit" "Bufetes" "Brètol" "Llanut" "Borinot" "Banyut" "Bagassa" "Beneit" "Llepaculs" "Nyèbit")
+    CAT = 0
+    ESP = 1
+    ENG = 2
+    NEP = 3
 
-incast = ("Malparido" "Bufetes" "Brutote" "Llano" "Tonto, simple" "Cuernudo" "Prostituta" "Bendito" "Lameculos" "Niñato")
+    insults = [
+        ["Malparit" "Bufetes" "Brètol" "Llanut" "Borinot" "Banyut" "Bagassa" "Beneit" "Llepaculs" "Nyèbit"],
+        ["Malparido" "Bufetes" "Brutote" "Llano" "Tonto, simple" "Cuernudo" "Prostituta" "Bendito" "Lameculos" "Niñato"],
+        ["Bastard" "Lawyers" "Brute" "Flat, Plain" "Silly" "Horned" "Prostitute" "Blessed" "Ass-licker" "Brat"],
+        ["बास्टर्ड" "वकील" "ब्रुट" "फ्लैट, प्लेन" "सिली" "सिङ्ग" "वेश्या" "धन्य" "गधा चाट्ने" "ब्याट"]
+    ]
 
-inangl = ("Bastard" "Lawyers" "Brute" "Flat, Plain" "Silly" "Horned" "Prostitute" "Blessed" "Ass-licker" "Brat")
+    # Bucle per demanar insults a l'usuari
 
-inklin = ("")
+
+
+
+
+    if ins in insults[CAT]:
+        index_insult_cat = insults[CAT].index(ins)
+
+        print(f"En català: {ins}")
+        print(f"En castellà: {insults[ESP][index_insult_cat]}")
+        print(f"En anglès: {insults[ENG][index_insult_cat]}")
+        print(f"En klingon: {insults[NEP][index_insult_cat]}")
+    else:
+        print("Insult no trobat. Prova'n un altre.")
+except ValueError:
+    print("No correct")
+
+
+
